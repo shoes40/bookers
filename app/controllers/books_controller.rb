58545c8bca8654cf25b091b67e0f90f0845396d8
shoes40 <105543830+shoes40@users.noreply.github.com>
2022-06-29 16:01:37 +0000

    提出課題２

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,12 +12,14 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
-      render :new
+      @books=Book.all
+      render :index
     end  
   end
   
   def index
     @books=Book.all
+    @book=Book.new
   end
   
   
@@ -31,15 +33,20 @@ class BooksController < ApplicationController
   
   def update
     @book=Book.find(params[:id])
-    @book.update(book_params)
+    if @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
     redirect_to book_path(@book.id)
+    else
+     render :edit,status: :unprocessable_entity
+    end  
+    
   end
   
   
   def destroy
    @book=Book.find(params[:id])  
    @book.destroy 
-   flash[:notice] = "Book was successfully created."
+   flash[:notice] = "Book was successfully destroyed."
    redirect_to '/books'  
   end
   
